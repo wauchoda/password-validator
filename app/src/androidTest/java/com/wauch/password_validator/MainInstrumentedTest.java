ce21d01c77e8fab3b87f3399a57116859fccade3
David Wauchope <wauchoda@users.noreply.github.com>
2018-02-09 16:30:58 -0400

    Added the tests which I forgot to include. Everything is now working :thumbsup:

--- app/src/androidTest/java/com/wauch/password_validator/MainInstrumentedTest.java
+++ app/src/androidTest/java/com/wauch/password_validator/MainInstrumentedTest.java
@@ -2,11 +2,23 @@ package com.wauch.password_validator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Rule;
 
+import java.net.PasswordAuthentication;
+
+import static android.support.test.espresso.Espresso.onView;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +28,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainInstrumentedTest {
+    @Rule
+    public ActivityTestRule<PasswordCheck> menuActivityTestRule =
+            new ActivityTestRule<>(PasswordCheck.class, true, true);
+
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +41,19 @@ public class MainInstrumentedTest {
 
         assertEquals("com.wauch.password_validator", appContext.getPackageName());
     }
+
+    @Test
+    public void testWeakInput() throws Exception {
+        onView(withId(R.id.editText)).perform(typeText("password"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Your password isn't strong enough.")));
+    }
+
+    @Test
+    public void testStrongInput() throws Exception {
+        onView(withId(R.id.editText)).perform(typeText("@ValidPassword103"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Strong Password")));
+    }
+
 }
