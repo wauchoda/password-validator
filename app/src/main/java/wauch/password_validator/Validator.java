9ab73b9ab1cbe42a797e508bd634a71da5b46dac
David Wauchope <wauchoda@users.noreply.github.com>
2018-02-08 22:43:57 -0400

    Added 3 more rules for password verification - checks for symbols in password - checks for numbers in password - checks for capitalization

--- app/src/main/java/wauch/password_validator/Validator.java
+++ app/src/main/java/wauch/password_validator/Validator.java
@@ -1,11 +1,20 @@
 package wauch.password_validator;
-
 /**
  * Created by wauch on 2018-02-08.
  * Tests the strength of an entered password.
  */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class Validator {
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private String REGEX;
 
     /**
      * @param password The inputted password to validate
@@ -15,9 +24,13 @@ public class Validator {
         int passedRules = 0;
         if(checkPassword(password))passedRules++;
         if(checkLength(password))passedRules++;
+        if(checkSymbol(password))passedRules++;
+        if(checkNumber(password))passedRules++;
+        if(checkCapital(password))passedRules++;
 
         return passedRules;
     }
+
 
     public boolean checkPassword(String password){
         return!password.equals("password");
@@ -25,5 +38,28 @@ public class Validator {
 
     public boolean checkLength(String password){
         return password.length() >= 8;
+    }
+
+    public boolean checkSymbol(String password){
+        REGEX = "\\W";
+        pattern = Pattern.compile(REGEX);
+        matcher = pattern.matcher(password);
+        return matcher.find();
+        //return pattern.matches("\\W", password);
+    }
+
+    public boolean checkNumber(String password) {
+        REGEX = "\\d";
+        pattern = Pattern.compile(REGEX);
+        matcher = pattern.matcher(password);
+        return matcher.find();
+        //return pattern.find("\\d", password);
+    }
+
+    public boolean checkCapital(String password) {
+        REGEX = "([a-z][A-Z])|([A-Z][a-z])";
+        pattern = Pattern.compile(REGEX);
+        matcher = pattern.matcher(password);
+        return matcher.find();
     }
 }
--- app/src/test/java/com/wauch/password_validator/ValidatorTest.java
+++ app/src/test/java/com/wauch/password_validator/ValidatorTest.java
@@ -14,7 +14,15 @@ import wauch.password_validator.Validator;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ValidatorTest {
-    Validator validtester = new Validator();
+    private Validator validtester = new Validator();
+
+    private String validPassword = "@ValidPassword10";
+    private String invalidPassword = "a";
+
+    @Test
+    public void ValidatorChecksValidator() throws Exception {
+        assertEquals(5, validtester.validatePassword(validPassword));
+    }
 
     @Test
     public void ValidatorChecksPasswordFalse() throws Exception {
@@ -22,17 +30,48 @@ public class ValidatorTest {
     }
     @Test
     public void ValidatorChecksPasswordTrue() throws Exception {
-        assertEquals(true, validtester.checkPassword("meme"));
+        assertEquals(true, validtester.checkPassword(validPassword));
     }
 
     @Test
     public void ValidatorChecksLengthTrue() throws Exception{
-        assertEquals(true, validtester.checkLength("longerthaneight"));
+        assertEquals(true, validtester.checkLength(validPassword));
 
     }
 
     @Test
     public void ValidatorChecksLengthFalse() throws Exception{
-        assertEquals(false, validtester.checkLength("short"));
+        assertEquals(false, validtester.checkLength(invalidPassword));
     }
+
+    @Test
+    public void ValidatorChecksSymbolTrue() throws Exception{
+        assertEquals( true, validtester.checkSymbol(validPassword));
+    }
+
+    @Test
+    public void ValidatorChecksSymbolFalse() throws Exception{
+        assertEquals( false, validtester.checkSymbol(invalidPassword));
+    }
+
+    @Test
+    public void ValidatorChecksNumberTrue() throws Exception{
+        assertEquals( true, validtester.checkNumber(validPassword));
+    }
+
+    @Test
+    public void ValidatorChecksNumberFalse() throws Exception{
+        assertEquals( false, validtester.checkNumber(invalidPassword));
+    }
+
+    @Test
+    public void ValidatorChecksCapitalTrue() throws Exception{
+        assertEquals(true, validtester.checkCapital(validPassword));
+    }
+
+    @Test
+    public void ValidatorChecksCapitalFalse() throws Exception{
+        assertEquals(false, validtester.checkCapital(invalidPassword));
+    }
+
 }
